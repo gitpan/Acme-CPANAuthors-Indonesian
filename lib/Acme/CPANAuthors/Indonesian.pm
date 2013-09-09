551c51843e49f14a09315c18063f880e5de60d73
Steven Haryanto <stevenharyanto@gmail.com>
2013-09-09 07:21:38 +0000

    Import of SHARYANTO/Acme-CPANAuthors-Indonesian-0.03 from CPAN.
    gitpan-cpan-distribution: Acme-CPANAuthors-Indonesian
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Acme-CPANAuthors-Indonesian-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Acme/CPANAuthors/Indonesian.pm
+++ lib/Acme/CPANAuthors/Indonesian.pm
@@ -1,11 +1,9 @@
 package Acme::CPANAuthors::Indonesian;
 
-# ABSTRACT: We are Indonesian CPAN authors (Kami para penulis CPAN)
-
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03'; # VERSION
 
 use Acme::CPANAuthors::Register (
     DNS         => 'Daniel Sirait',
@@ -15,18 +13,19 @@ use Acme::CPANAuthors::Register (
 );
 
 1;
+# ABSTRACT: We are Indonesian CPAN authors (Kami para penulis CPAN Indonesia)
 
-
+__END__
 
 =pod
 
 =head1 NAME
 
-Acme::CPANAuthors::Indonesian - We are Indonesian CPAN authors (Kami para penulis CPAN)
+Acme::CPANAuthors::Indonesian - We are Indonesian CPAN authors (Kami para penulis CPAN Indonesia)
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
@@ -57,17 +56,13 @@ L<Acme::CPANAuthors::Chinese> - Code and documentation nearly taken verbatim fro
 
 =head1 AUTHOR
 
-  Steven Haryanto <stevenharyanto@gmail.com>
+Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2009 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
-
-__END__
-
